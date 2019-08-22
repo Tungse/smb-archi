53482dde6b6980816edcd597c2bb6cf3afaf3fec
Tungse <t.pham@stroeermediabrands.de>
2019-08-22 14:01:14 +0200

    add mongo prod

--- src/index.js
+++ src/index.js
@@ -135,5 +135,5 @@ cron.schedule('* * * * *', () => {
 })
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!!')
 })
